2210f6e3f94e36a22efb2759c3ffa8f01eb42889
basti <bastian_behrendt@yahoo.de>
2024-04-08 14:55:50 +0100

    Initial commit

---
+++ src/main/java/com/example/rickandmortyrest/RickAndMortyRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.rickandmortyrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rickandmortyrest/RickAndMortyRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rickandmortyrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickAndMortyRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
